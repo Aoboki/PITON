489057f1e8f4976df7d75ecb17ee2ab0258f8648
Aoboki <129741770+Aoboki@users.noreply.github.com>
2026-06-18 15:46:19 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+def close_window():
+    root.destroy()
+
+root = tk.Tk()
+root.title("Моё окно")
+root.geometry("300x150")
+
+button = tk.Button(
+    root,
+    text="Закрыть",
+    command=close_window
+)
+
+button.pack(expand=True)
+
+root.mainloop()
